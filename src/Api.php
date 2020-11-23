df1eeee3e5d1d02205ded1a44417729937634dfb
Kevin Gautreau <contact@kgaut.net>
2020-11-23 14:11:09 +0100

    API - methods listProjects and showProject

--- src/Api.php
+++ src/Api.php
@@ -6,6 +6,7 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\ImmutableConfig;
 use Drupal\gitlab_api\Entity\GitlabServer;
 use Github\Client;
+use Github\ResultPager;
 
 class Api {
 
@@ -19,8 +20,39 @@ class Api {
 
   protected function init() {
     if (!isset($this->client)) {
-      $this->client->authenticate($this->config->get('token'), NULL, Client::AUTH_ACCESS_TOKEN);
+      $this->client = new Client();
+      $result = $this->client->authenticate($this->config->get('token'), NULL, Client::AUTH_ACCESS_TOKEN);
     }
+  }
+
+  /**
+   * Returns all projects on the server
+   *
+   * @param bool $simple
+   * @param bool $includeArchived
+   * @param array $additionalParams
+   *
+   * @return array
+   * @throws \Http\Client\Exception
+   */
+  public function listProjects(bool $simple = TRUE, $includeArchived = FALSE, $additionalParams = []) : array {
+    $this->init();
+    $paginator  = new ResultPager($this->client);
+    $parameters = [];
+
+    return $paginator->fetchAll($this->client->api('user'), 'myRepositories', $parameters);
+  }
+
+  /**
+   * Returns information about a given project.
+   *
+   * @param $project_id
+   *
+   * @return array
+   */
+  public function showProject($project_id) : array {
+    $this->init();
+    return $this->client->repository()->showById($project_id);
   }
 
 }
